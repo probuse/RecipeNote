8a62dad6be006a69d634da7612de025e5ca1648e
probuse <nprobuse@gmail.com>
2017-10-30 15:31:49 +0300

    changed the html for the login route to login_.html and register to register1.html

--- recipe.py
+++ recipe.py
@@ -15,12 +15,12 @@ def index():
 @app.route('/register')
 def register():
     "Renders the index page"
-    return render_template('register.html')
+    return render_template('register1.html')
 
 @app.route('/login')
 def login():
     "Renders login page"
-    return render_template('login.html')
+    return render_template('login_.html')
 
 @app.route('/forgot-password')
 def forgot_password():
