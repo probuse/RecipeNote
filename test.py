02f433429d718967c00be441c783dd0296b913e1
probuse <nprobuse@gmail.com>
2017-10-31 16:31:00 +0300

    added tests to test if recipes_add, recipe_edit, recipe_detail, category, category_create, category_edit loads and contains right content

--- test.py
+++ test.py
@@ -53,6 +53,81 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/recipes', content_type='html/text')
         self.assertIn(b'Recipes', response.data)
 
+    def test_recipes_add_page_loads(self):
+        "tests to see that the recipes_add page loads correctly"
+        tester = app.test_client(self)
+        response = tester.get('/recipes_add', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipes_add_page_contains_right_content(self):
+        "Tests if the recipes_add page contains the right content"
+        tester = app.test_client(self)
+        response = tester.get('/recipes_add', content_type='html/text')
+        self.assertIn(b'Create a new Recipe', response.data)
+
+    def test_recipe_edit_page_loads(self):
+        "tests to see that the recipe_edit page loads correctly"
+        tester = app.test_client(self)
+        response = tester.get('/recipe_edit', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_edit_page_contains_right_content(self):
+        "Tests if the recipe_edit page contains the right content"
+        tester = app.test_client(self)
+        response = tester.get('/recipe_edit', content_type='html/text')
+        self.assertIn(b'Edit Your Recipe', response.data)
+
+    def test_recipe_detail_page_loads(self):
+        "tests to see that the recipe_detail page loads correctly"
+        tester = app.test_client(self)
+        response = tester.get('/recipe_detail', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_detail_page_contains_right_content(self):
+        "Tests if the recipe_detail page contains the right content"
+        tester = app.test_client(self)
+        response = tester.get('/recipe_detail', content_type='html/text')
+        self.assertIn(b'Recipe Details', response.data)
+
+    def test_category_page_loads(self):
+        "tests to see that the category page loads correctly"
+        tester = app.test_client(self)
+        response = tester.get('/category', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_page_contains_right_content(self):
+        "Tests if the category page contains the right content"
+        tester = app.test_client(self)
+        response = tester.get('/category', content_type='html/text')
+        self.assertIn(b'Your Categories', response.data)
+
+    def test_category_edit_page_loads(self):
+        "tests to see that the category_edit page loads correctly"
+        tester = app.test_client(self)
+        response = tester.get('/category_edit', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_edit_page_contains_right_content(self):
+        "Tests if the category_edit page contains the right content"
+        tester = app.test_client(self)
+        response = tester.get('/category_edit', content_type='html/text')
+        self.assertIn(b'Edit Category', response.data)
+
+    def test_category_create_page_loads(self):
+        "tests to see that the category_create page loads correctly"
+        tester = app.test_client(self)
+        response = tester.get('/category_create', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_create_page_contains_right_content(self):
+        "Tests if the category_create page contains the right content"
+        tester = app.test_client(self)
+        response = tester.get('/category_create', content_type='html/text')
+        self.assertIn(b'Create a new Category', response.data)
+    
+
+
+
     
 if __name__ == "__main__":
     unittest.main()
