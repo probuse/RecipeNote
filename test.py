6f5ddb8cccf892759f0795430f6ca3ea5293312c
probuse <nprobuse@gmail.com>
2017-11-03 09:02:29 +0300

    re-added tests for register and login pages

--- test.py
+++ test.py
@@ -17,6 +17,30 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/', content_type='html/text')
         self.assertIn(b'Are you into preparing your food?', response.data)
 
+    def test_register_page_loads(self):
+        "tests to see that the register page loads correctly"
+        tester = app.test_client(self)
+        response = tester.get('/register', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page_contains_right_content(self):
+        "Tests if the register page contains the right content"
+        tester = app.test_client(self)
+        response = tester.get('/register', content_type='html/text')
+        self.assertIn(b'Please Register to continue', response.data)
+
+    def test_login_page_loads(self):
+        "tests to see that the login page loads correctly"
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_contains_right_content(self):
+        "Tests if the login page contains the right content"
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertIn(b'Please Login to continue', response.data)
+
     def test_recipes_page_loads(self):
         "tests to see that the recipes page loads correctly"
         tester = app.test_client(self)
