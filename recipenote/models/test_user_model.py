0d883836e6951a160b0eb4cf4b74948af90c5cae
probuse <nprobuse@gmail.com>
2017-11-06 16:38:11 +0300

    initial commit of user test model

---
+++ recipenote/models/test_user_model.py
@@ -0,0 +1,45 @@
+"""All tests for our models go here."""
+import unittest
+
+
+class UserModelTestCase(unittest.TestCase):
+    "tests features and funtionlity for user.py"
+    
+    def setUp(self):
+        pass
+
+    def test_user_has_creates_category(self):
+        "Tests to see user has created a category"
+        pass
+
+    def test_user_can_edit_category_name(self):
+        "Tests user can edit category name"
+        pass
+
+    def test_user_can_delete_category(self):
+        "Tests user can delete category"
+        pass
+
+    def test_user_can_add_recipe(self):
+        "Tests user is able to add recipe"
+        pass
+
+    def test_user_can_edit_recipe_name(self):
+        "Test user can edit recipe name"
+        pass
+
+    def test_user_can_edit_recipe_prep_method(self):
+        "Test user can edit prep_method"
+        pass
+
+    def test_user_can_edit_recipe_ingredients(self):
+        "Test user can edit recipe ingredients"
+        pass
+    
+    def test_can_delete_recipe(self):
+        "Tests user can delete recipe"
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+    
