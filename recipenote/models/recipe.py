206a2bd0c84b1b8614eb1085df0d991ecf1faa45
probuse <nprobuse@gmail.com>
2017-11-07 19:53:38 +0300

    added prep_method attribute

--- recipenote/models/recipe.py
+++ recipenote/models/recipe.py
@@ -4,5 +4,6 @@
 class Recipe(object):
     "Controls Recipe object"
 
-    def __init__(self, name):
-        self.name = name    
+    def __init__(self, name, prep_method):
+        self.name = name
+        self.prep_method = prep_method    
