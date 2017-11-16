ef816a30317b68756bb6cd96ca6e270eec1881b9
probuse <nprobuse@gmail.com>
2017-11-16 22:30:48 +0300

    added prep_method argument

--- recipenote/models/recipe.py
+++ recipenote/models/recipe.py
@@ -4,6 +4,7 @@
 class Recipe(object):
     "Controls Recipe object"
 
-    def __init__(self, name, prep_method):
+    def __init__(self, name, category, prep_method):
         self.name = name
+        self.category = category
         self.prep_method = prep_method    
