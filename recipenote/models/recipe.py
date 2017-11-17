4faba2468fc341d97b9687f53146d110a613e3c0
probuse <nprobuse@gmail.com>
2017-11-17 10:59:45 +0300

    removed category attribute

--- recipenote/models/recipe.py
+++ recipenote/models/recipe.py
@@ -4,7 +4,6 @@
 class Recipe(object):
     "Controls Recipe object"
 
-    def __init__(self, name, category, prep_method):
+    def __init__(self, name, prep_method):
         self.name = name
-        self.category = category
         self.prep_method = prep_method    
